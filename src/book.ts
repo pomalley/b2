d3e53bbe931befec2ac89f2b771878718f418555
Peter O'Malley <ooomalley@gmail.comm>
2016-12-11 16:25:35 -0500

    Basically kinda working autofields

--- src/book.ts
+++ src/book.ts
@@ -2,31 +2,68 @@
  * Class for dealing with a book object.
  */
 
+// TODO(pomalley): refactor autofield into new file
 interface FieldObj {
   label: string;
   value: any;
 }
 
-@component("auto-fielded")
+interface AutofieldConfig {
+  label: string;
+  path: string;
+  propName: string;
+}
+
+// @component("auto-fielded")
 // @template("")
 class Autofielded extends polymer.Base {
+  // @property()
+  public autofields: FieldObj[];
+  public autoconfigs: AutofieldConfig[];
+  public addField(fieldConfig: AutofieldConfig) {
+    this.autoconfigs = this.autoconfigs || [];
+    this.autoconfigs.push(fieldConfig);
+  }
+
   @property()
-  private autofields: FieldObj[];
-  public addField(fieldObj: FieldObj) {
-    this.autofields = this.autofields || [];
-    // this.push("autofields", fieldObj);
-    this.autofields.push(fieldObj);
-    console.log(fieldObj);
-    console.log(this.autofields);
+  protected isReady: boolean;
+
+  constructor() {
+    super();
+    this.autofields = [];
+    for (let autoconfig of this.autoconfigs) {
+      this.push("autofields", {label: autoconfig.label, value: "a"});  // TODO(pomalley): value is overwritten, unneeded
+    }
   }
 }
 // Autofielded.register();
 
 function autofield(fieldObj: {label: string, path: string}) {
   return (target: Autofielded, name: string) => {
-    target.addField({label: fieldObj.label, value: 1});
-    // target.addField({label: fieldObj.label, value: target[fieldObj.path]});
-    // chain the @property decorator on this.
+    target.addField({label: fieldObj.label, path: fieldObj.path, propName: name});
+    const n = target.autoconfigs.length - 1;
+    // TODO(pomalley): clean this up and move these functions into addField (?)
+    target["__autoObserve" + name] = function(newValue) {
+      // console.log("auto obs of " + name + " new: " + newValue);
+      this.set(fieldObj.path, newValue);
+    };
+    observe(name)(target, "__autoObserve" + name);
+
+    target["__autoObserve" + fieldObj.path] = function(newValue) {
+      // console.log("auto obs of " + fieldObj.path + " new: " + newValue);
+      this.set(name, newValue);
+      this.set("autofields.#" + n + ".value", newValue);
+    };
+    observe(fieldObj.path)(target, "__autoObserve" + fieldObj.path);
+
+    target["__autoObserve" + n] = function(newValue) {
+      // console.log("auto obs # " + n + " new: " + newValue + " (isready: " + this.isReady + ")");
+      if (this.isReady) {
+        this.set(fieldObj.path, newValue);
+      }
+    };
+    observe("autofields.#" + n + ".value")(target, "__autoObserve" + n);
+    // chain the property decorator
     return property()(target, name);
   };
 }
@@ -49,18 +86,17 @@ class Book extends Autofielded {
   private formattedText: string;
 
   @autofield({label: "Title", path: "book.title"})
-  private Title: String;
+  private Title: string;
 
-  @observe("fields.#0.value")
-  private listTitleChanged(newValue) {
-    this.set("book.title", newValue);
-  }
+  @autofield({label: "Authors", path: "book.authors"})
+  private authors: string;
+
+  @autofield({label: "text", path: "book.text"})
+  private text: string;
 
   @observe("book")
   private bookChanged(book) {
-    console.log("book changed");
-    // this.fields = [{value: this.book.title, label: "Title"}];
-    this.linkPaths("autofields.#0.value", "book.title");
+    console.log("book changed: ", book.title + "|" + book.authors);
   }
 
   private _formatTitle(title) {
@@ -75,11 +111,6 @@ class Book extends Autofielded {
   private _formatText(text) {
     return text || "[no text]";
   }
-
-  public ready() {
-    this.addField({label: "InReady", value: "hum"});
-  }
-
 }
 
 Book.register();
